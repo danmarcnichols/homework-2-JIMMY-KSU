2ab61d82f51cfb2ff159a96be97f16b50fb5e429
Daniel Nichols <danmarcnichols@gmail.com>
2018-09-11 12:41:55 -0500

    Add kelvin into script

--- temperature.sh
+++ temperature.sh
@@ -2,3 +2,6 @@
 F=$1
 C=$(echo "scale=2;($F-32)*5/9" | bc)
 echo "$F degree Fahreheit is equivalent to $C degree Celsius"
+K=$(echo "scale=2;($C+273.15)" | bc)
+echo "$F degree Fahreheit is equivalent to $C degree Celsius and $K degreee Kelvin"
+
